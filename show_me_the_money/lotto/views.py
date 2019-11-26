c32683e18e7d3bf944a0be3aa6fa5fd5d8d93b3d
niceAha <mistel1327@gmail.com>
2019-11-27 01:14:16 +0900

    Add Comments in parse_lotto.py & Add Bootstrap in templates & Add Bootstrap static files in project

--- show_me_the_money/lotto/views.py
+++ show_me_the_money/lotto/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from . import parse_lotto
 
 # Create your views here.
 
 def index(request):
-    return ''
+    return render(request, 'index.html')
+
+# lotto_data_test
+#       This function is for testing lotto_data
+
+def lotto_data_test(request):
+    lotto_data = parse_lotto.get_lotto_data(parse_lotto.get_lotto_cnt())
+    
+    lotto_number_sum = parse_lotto.get_lotto_number_sum(lotto_data)
+    lotto_number_win_count = parse_lotto.get_lotto_number_win_count(lotto_data)
+    lotto_number_each_win_count = parse_lotto.get_lotto_number_each_win_count(lotto_data)
+
+    context = {
+        'lotto_number_sum' : lotto_number_sum,
+        'lotto_number_win_count' : lotto_number_win_count,
+        'lotto_number_each_win_count' : lotto_number_each_win_count
+    }
+
+    return render(request, 'test/lotto_data.html', context)
