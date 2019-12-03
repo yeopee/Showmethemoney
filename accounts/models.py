60f0af16e37cb89b0198bf2dfbfc8ace1839de8c
niceAha <mistel1327@gmail.com>
2019-12-03 13:48:01 +0900

    Edit user models for user.info.get.money

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth import settings
 
 # Create your models here.
 
+# userinfo models
+# 
+# 1. how to use in html page
+#       user.info.get.money
+#       user.info.get.date
+
 class UserInfo(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='info', on_delete=models.CASCADE)
 
